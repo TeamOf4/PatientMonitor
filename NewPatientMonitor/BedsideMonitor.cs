702a35ac885b0ebef7541407c8ce6d4983a0e1c6
unknown <sjs281@A07452.ANGLIA.LOCAL>
2015-11-12 16:23:09 +0000

    Updated bedside monitor test class

---
+++ NewPatientMonitor/BedsideMonitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPatientMonitor
+{
+    public class BedsideMonitor
+    {
+        public List<IModule> Bedsidemodules
+        {
+            get;
+            set;
+        }
+
+        public void AddMonitor(IModule moduleToBeAdded)
+        {
+            Bedsidemodules.Add(moduleToBeAdded);
+        }
+    }
+}
---
+++ NewPatientMonitor/IModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NewPatientMonitor
+{
+    public interface IModule
+    {
+        float LowerLimit { get; set; }
+        string Name { get; set; }
+        float UpperLimit { get; set; }
+    }
+}
