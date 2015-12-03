aab0fbdb17b16377c11dc93b08c343157a20d085
unknown <sjs281@A07452.ANGLIA.LOCAL>
2015-12-03 12:04:00 +0000

    Added user interface again

---
+++ NewPatientMonitor/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewPatientMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Change_limits change = new Change_limits();
+            change.Show();  
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Alarm alarm = new Alarm();
+            alarm.Show();
+        }
+    }
+}
