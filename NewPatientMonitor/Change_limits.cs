dae29bfa5d98d1114e418bca019e2bc708b549a5
unknown <sjs281@A07452.ANGLIA.LOCAL>
2015-12-03 12:08:18 +0000

    Added more UI

---
+++ NewPatientMonitor/Change_limits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewPatientMonitor
+{
+    public partial class Change_limits : Form
+    {
+        public Change_limits()
+        {
+            InitializeComponent();
+        }
+
+        private void Change_limits_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void textBox17_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
