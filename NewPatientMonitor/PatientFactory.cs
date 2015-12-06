5d840055f8faba0408dc0469678bd21a76c15472
Jack <jack.w.sanger@gmail.com>
2015-12-06 10:43:23 +0000

    Added patient factory class

--- NewPatientMonitor/PatientFactory.cs
+++ NewPatientMonitor/PatientFactory.cs
@@ -6,7 +6,30 @@ namespace NewPatientMonitor
     {
         public object CreateAndReturnObj(PatientClassesEnumeration objectToGet)
         {
-            throw new NotImplementedException();
+            object createdObject = null;
+
+            switch (objectToGet)
+            {
+                case PatientClassesEnumeration.PatientAlarmer:
+                    PatientAlarmer alarmer = new PatientAlarmer();
+                    createdObject = alarmer;
+                    break;
+                case PatientClassesEnumeration.PatientDataReader:
+                    PatientDataReader dataReader = new PatientDataReader();
+                    createdObject = dataReader;
+                    break;
+                case PatientClassesEnumeration.PatientData:
+                    PatientData patientData = new PatientData();
+                    createdObject = patientData;
+                    break;
+                case PatientClassesEnumeration.PatientDataWriter:
+                    PatientDataWriter dataWriter = new PatientDataWriter();
+                    createdObject = dataWriter;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid parameter passed");
+            }
+            return createdObject;
         }
     }
 }
