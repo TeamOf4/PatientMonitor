59f46ba032f3de3c5d8e8d9602eab2acc2495339
Jack <jack.w.sanger@gmail.com>
2015-12-06 10:44:21 +0000

    Added new test to TestPatientAlarmer

--- NewPatientMonitorTest/TestPatientAlarmer.cs
+++ NewPatientMonitorTest/TestPatientAlarmer.cs
@@ -121,5 +121,31 @@ namespace NewPatientMonitorTest
 
             Assert.IsFalse(alarmCalled);
         }
+
+        [TestMethod()]
+        public void ReadingTestAlarmWasCalled()
+        {
+            var alarmCalled = true;
+
+            var testModule = new Mock<IModule>();
+
+            testModule.Setup(a => a.Name).Returns("Test Module");
+            testModule.Setup(b => b.LowerLimit).Returns(5f);
+            testModule.Setup(c => c.UpperLimit).Returns(10f);
+
+            var testReadings = new Mock<IPatientData>();
+            var values = new List<float>(4) { 7f, 6f, 5f, 4f };
+            testReadings.Setup(a => a.Values).Returns(values);
+
+            var testMonitor = new Mock<IBedsideMonitor>();
+            testMonitor.Setup(a => a.BedsideModules).Returns(new List<IModule>(4) { new Module(testModule.Object), new Module(testModule.Object), new Module(testModule.Object), new Module(testModule.Object), });
+
+            IPatientAlarmer testAlarmer = new PatientAlarmer();
+
+            testAlarmer.ReadingTest(testReadings.Object, testMonitor.Object);
+            testAlarmer.ModuleAlarm += (sender, e) => alarmCalled = false;
+
+            Assert.IsTrue(alarmCalled);
+        }
     }
 }
