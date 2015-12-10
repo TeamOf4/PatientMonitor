c0308b8fcc2b5ee46e8342dcb58ab1c84971ed89
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 09:19:43 +0000

    Imported updown from original application

---
+++ NewPatientMonitor/updown.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NewPatientMonitor
+{
+    /// <summary>
+    /// Interaction logic for UpDown.xaml
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        Label textValue;
+
+        public event EventHandler ValueChanged;
+
+        private int _numValue = 0;
+        public int AlarmValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                textValue.Content = value.ToString();
+            }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            textValue.Content = "0";
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            textValue.Content = ++AlarmValue;
+            if (ValueChanged != null) ValueChanged(this, null);
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            textValue.Content = --AlarmValue;
+            if (ValueChanged != null) ValueChanged(this, null);
+        }
+    }
+}
