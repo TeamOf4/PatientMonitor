881777165aa2533f7fd8cb4324c6dcdbe60ea2d3
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 09:20:20 +0000

    Imported Mainwindow.xaml.cs from original application

--- NewPatientMonitor/MainWindow.xaml.cs
+++ NewPatientMonitor/MainWindow.xaml.cs
@@ -1,37 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Media;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NewPatientMonitor
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+
+
+
     public partial class MainWindow : Window
     {
+
         public List<Label> Modules;
-        public object patientSelector;
+        public ComboBox patientSelector;
+        SoundPlayer mutable = new SoundPlayer(NewPatientMonitor.Properties.Resources.Mutable);
+        SoundPlayer nonMutable = new SoundPlayer(NewPatientMonitor.Properties.Resources.NonMutable);
 
         public MainWindow()
         {
             InitializeComponent();
+            PatientFactory factory = new PatientFactory();
+            PatientMonitoringController controller = new PatientMonitoringController(this, factory);
+            controller.RunMonitor();
         }
 
-        public CheckBox AlarmMute { get; set; }
-
-        public void soundMutableAlarm()
+        public void SoundMutableAlarm()
         {
-            throw new NotImplementedException();
+            mutable.Stop();
+            mutable.Play();
+        }
+
+        public void SoundNonMutableAlarm()
+        {
+            nonMutable.Stop();
+            nonMutable.Play();
         }
     }
 }
