5e6baaff3b5a738c5d4c9a0ad2e45e357bb47a17
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 15:34:52 +0000

    Added comment to IPatientAlarmer

--- NewPatientMonitor/IPatientAlarmer.cs
+++ NewPatientMonitor/IPatientAlarmer.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+* Stores a list of alarm testers
+* Contains an event handler event for the module
+* Reads patient data, if the reading value is outside the limits (determined by alarm tester)
+* then Module alarm is set to null J.S 
+*/
+using System;
 using System.Collections.Generic;
 
 namespace NewPatientMonitor
