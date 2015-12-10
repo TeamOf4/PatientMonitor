7ad5a8d6656f87ef23410524bcd511b13b04093a
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 09:20:49 +0000

    added class to test controller

---
+++ NewPatientMonitorTest/TestPatientMonitoringController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NewPatientMonitor;
+
+namespace NewPatientMonitorTest
+{
+    [TestClass()]
+    public class TestPatientMonitoringController
+    {
+        [TestMethod()]
+        public void TestPatientMonitoringControllerConstructor()
+        {
+            Mock<IPatientFactory> testFactory = new Mock<IPatientFactory>();
+            Mock<IPatientData> testPatient = new Mock<IPatientData>();
+            testPatient.Setup(a => a.Values).Returns(() => new List<float> {12f, 13f, 14f, 15f});
+             
+            
+            testFactory.Setup(a => a.CreateAndReturnObj(PatientClassesEnumeration.PatientData)).Returns(testPatient);
+
+            Assert.Fail();
+        }
+    }
+}
