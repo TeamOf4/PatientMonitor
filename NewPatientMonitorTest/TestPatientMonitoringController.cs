95f6dae899c7671f71e5fb4331a17ff3ecdafe23
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 15:00:44 +0000

    Added test class for controller

---
+++ NewPatientMonitorTest/TestPatientMonitoringController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewPatientMonitor;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPatientMonitorTest
+{
+    [TestClass()]
+    public class TestPatientMonitoringController
+    {
+        [TestMethod()]
+        public void TestRunMonitor()
+        {
+            MainWindow testWindow = null;
+            Mock<IPatientFactory> testFactory = new Mock<IPatientFactory>();
+            IPatientMonitoringController testController = new PatientMonitoringController(testWindow, testFactory.Object);
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestRunMonitor1()
+        {
+            Mock<MainWindow> testWindow = new Mock<MainWindow>();
+            Mock<IPatientFactory> testFactory = new Mock<IPatientFactory>();
+            IPatientMonitoringController testController = new PatientMonitoringController(testWindow.Object, testFactory.Object);
+            testController.RunMonitor();
+            Assert.Fail();
+        }
+    }
+}
