5dc6921bd7acc09728948dce22e97ea179d7ab5b
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 16:10:06 +0000

    added comments to IPatientMonitoringController

--- NewPatientMonitor/IPatientMonitoringController.cs
+++ NewPatientMonitor/IPatientMonitoringController.cs
@@ -1,4 +1,8 @@
-﻿namespace NewPatientMonitor
+﻿/* Interface for the PatientMonitoring Controller
+* Runs the patient monitor J.S
+*/
+
+namespace NewPatientMonitor
 {
     public interface IPatientMonitoringController
     {
