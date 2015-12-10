9a87a748691585ea1c5d2b70d42eac8ea76f17e8
Jack Sanger <jack.sanger@student.anglia.ac.uk>
2015-12-10 14:26:01 +0000

    Added Test class for numericUpDown

---
+++ NewPatientMonitorTest/TestNumericUpDown.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewPatientMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPatientMonitorTest
+{
+    [TestClass()]
+    public class TestNumericUpDown
+    {
+        [TestMethod()]
+        public void TestNumericUpDownConstructor()
+        {
+            
+            Assert.Fail();
+        }
+    }
+}
